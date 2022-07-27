87e37ab01a4b912587a0a49d88a9e9d29fa4546a
Denis <denismunene999@gmail.com>
2022-07-27 12:31:59 +0300

    add a post event button

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './Events.css'
 
 function Events() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8000/events")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  }, [])
+
+
   return (
-    <div>Events</div>
+    <div>
+      <div>
+        <img src='https://thumbs.dreamstime.com/b/silhouettes-concert-crowd-front-bright-stage-lights-64551199.jpg' alt='events' className='image' />
+        <div className='image-text'>
+          <div>
+            <h2 className='title'>Upcoming events</h2>
+          </div>
+          <button className='btn'>Post an event</button>
+        </div>
+      </div>
+      <h2>Events hub</h2>
+    </div>
   )
 }
 
