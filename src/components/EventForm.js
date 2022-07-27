b535b9b7e92dc7a4f9ec8ce8ba6b7256e7042bf6
Denis <denismunene999@gmail.com>
2022-07-27 23:42:59 +0300

    set up controlled form

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './EventForm.css'
 
 function EventForm() {
   const [formData, setFormData] = useState({
@@ -10,37 +11,31 @@ function EventForm() {
     location: ""
   })
 
-  function handleChange(e) {
-    console.log("changed")
+  //update form data
+  function handleChange(event) {
+    const name = event.target.name;
+    let value = event.target.value;
+
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   }
 
   return (
-    <form className='new-event'>
-      <label>
-        Name:
-        <input type='text' name='name' onChange={handleChange} value={formData.name} />
-      </label>
-      <label>
-        Image:
-        <input type='text' name='image' onChange={handleChange} value={formData.image} />
-      </label>
-      <label>
-        Date:
-        <input type='text' name='date' onChange={handleChange} value={formData.date} />
-      </label>
-      <label>
-        Price:
-        <input type='text' name='price' onChange={handleChange} value={formData.price} />
-      </label>
-      <label>
-        Venue:
-        <input type='text' name='venue' onChange={handleChange} value={formData.venue} />
-      </label>
-      <label>
-        Location:
-        <input type='text' name='location' onChange={handleChange} value={formData.location} />
-      </label>
+    <div>
+      <h2>Add event:</h2>
+      <form className='new-event'>
+        <input placeholder='Name' type='text' name='name' onChange={handleChange} value={formData.name} />
+        <input placeholder='Image' type='text' name='image' onChange={handleChange} value={formData.image} />
+        <input placeholder='Date' type='text' name='date' onChange={handleChange} value={formData.date} />
+        <input placeholder='Price' type='number' name='price' onChange={handleChange} value={formData.price} />
+        <input placeholder='Venue' type='text' name='venue' onChange={handleChange} value={formData.venue} />
+        <input placeholder='Location' type='text' name='location' onChange={handleChange} value={formData.location} />
+        <input type='submit' value='Post event' />
     </form>
+    </div>
   )
 }
 
