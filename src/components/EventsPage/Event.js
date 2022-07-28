879d91b6404aa0a97a58a3ce68898846c1e64aaa
Denis <denismunene999@gmail.com>
2022-07-28 21:33:27 +0300

    add nested routes for the events

--- src/components/EventsPage/Event.js
+++ src/components/EventsPage/Event.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import './Event.css'
 
 function Event({ dataObj }) {
@@ -7,29 +8,32 @@ function Event({ dataObj }) {
 
   const dateArray = date.split(" ")
 
+
   return (
     // create a card for each object 
-    <div className='card'>
-      <img className='card-image' src={image} alt='pic' />
-      <p className='price'>{price}</p>
-      <div className='card-body'>
-        <div className='date'>
-          <h4 className='month'>{dateArray[0]}</h4>
-          <p className='day'>{dateArray[1]}</p>
+    <Link to={`/events/${dataObj.id}`} style={{ textDecoration: 'none' }} >
+      <div className='card' >
+        <img className='card-image' src={image} alt='pic' />
+        <p className='price'>{price}</p>
+        <div className='card-body'>
+          <div className='date'>
+            <h4 className='month'>{dateArray[0]}</h4>
+            <p className='day'>{dateArray[1]}</p>
+          </div>
+          <div className='description'>
+            <h3 className='name'>{name}</h3>
+            <p className='venue'>{venue}</p>
+            <p className='location'>{location}</p>
+          </div>
         </div>
-        <div className='description'>
-          <h3 className='name'>{name}</h3>
-          <p className='venue'>{venue}</p>
-          <p className='location'>{location}</p>
+        <div className='socio'>
+            <i className="fa-brands fa-square-facebook"></i>
+            <i className="fa-brands fa-square-twitter"></i>
+            <i className="fa-brands fa-square-instagram"></i>
+            <i className="fa-brands fa-linkedin"></i>
         </div>
       </div>
-      <div className='socio'>
-          <i className="fa-brands fa-square-facebook"></i>
-          <i className="fa-brands fa-square-twitter"></i>
-          <i className="fa-brands fa-square-instagram"></i>
-          <i className="fa-brands fa-linkedin"></i>
-      </div>
-    </div>
+    </Link>
   )
 }
 
