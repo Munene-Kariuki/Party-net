749f118deed8e9afb3e4d364d637ec37c1651a4f
Denis <denismunene999@gmail.com>
2022-07-29 18:24:55 +0300

    delete unused variables

--- src/components/EventsPage/Events.js
+++ src/components/EventsPage/Events.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Route, Routes, Router, useLocation} from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import './Events.css'
 import Event from './Event'
 import Filter from './Filter'
